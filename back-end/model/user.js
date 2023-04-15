4fe1217b6bd5535ba7ea99fbc0d6a0ec548f0a2a
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-15 13:47:43 +0330

    refactor: Improve user model
    improve user model for remove _id from parameter

--- back-end/model/user.js
+++ back-end/model/user.js
@@ -2,22 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    firstName: { type: { fa: String, en: String }, required: true },
-    lastName: { type: { fa: String, en: String }, required: true },
-    job: { type: { fa: String, en: String }, required: true },
-    address: { type: { fa: String, en: String }, required: true },
-    bio: { type: { fa: String, en: String }, required: true },
-    gender: { type: { fa: String, en: String }, required: true },
+    firstName: { fa: { type: String, required: true }, en: { type: String, required: true } },
+    lastName: { fa: { type: String, required: true }, en: { type: String, required: true } },
+    job: { fa: { type: String, required: true }, en: { type: String, required: true } },
+    address: { fa: { type: String, required: true }, en: { type: String, required: true } },
+    bio: { fa: { type: String, required: true }, en: { type: String, required: true } },
+    gender: { fa: { type: String, required: true }, en: { type: String, required: true } },
     age: { type: Number, required: true },
     email: { type: String, required: true },
     phone: { type: Number, required: true },
-    socialsNetwork: {
-        type: {
-            github: String,
-            linkedin: String,
-            instagram: String
-        }, required: false
-    },
+    socialsNetwork: { type: Map, required: false },
     avatar: { type: String },
 })
 
