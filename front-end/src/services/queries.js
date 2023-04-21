75a6ed506ca354118b06c51ad66c637312eb84cd
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-21 22:40:11 +0330

    Front-end feat: create getExperiences query

--- front-end/src/services/queries.js
+++ front-end/src/services/queries.js
@@ -3,4 +3,9 @@ import service from "./Api";
 export const getAbout = async () => {
     const res = await service.get('/api/about/getAbout')
     return res?.data;
+}
+
+export const getExperiences = async () => {
+    const res = await service.get('/api/experience/getExperiences')
+    return res?.data;
 } 
