87040b5c20f2e050d753bc785cfa9012d6d0030d
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-22 16:44:25 +0330

    Front-end feat: Create EducatinItem and EducationItemShimmer
    show educations in resume page

---
+++ front-end/src/components/educationItem/EducationItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const EducationItem = ({ item }) => {
+    return (
+        <div className='w-full flex group'>
+            <div className="w-44 min-w-[176px] text-end">
+                <p className="text-xs text-captionDark font-semibold pt-0.5">{item?.dateStart} - {item?.dateEnd}</p>
+                <p className="text-xs text-captionLight pt-0.5">{item?.location}</p>
+            </div>
+            <div className="border-r relative mx-6">
+                <span className="absolute top-1.5 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-white rounded-full border-2 border-primary ring-[5px] ring-primary/30"></span>
+            </div>
+            <div className="pb-7 group-last:pb-0 w-full">
+                <p className="text-base text-captionDark font-semibold capitalize">{item?.title}</p>
+                <p className="text-sm text-caption mt-2">{item?.university}</p>
+                <p className="text-sm text-caption mt-2">{item?.grade}</p>
+            </div>
+        </div>
+    )
+}
+
+export default EducationItem
--- front-end/src/containers/resume/Resume.js
+++ front-end/src/containers/resume/Resume.js
@@ -1,10 +1,12 @@
 import Title from "@/components/titlePage/Title"
 import useTransition from "@/hooks/useTransition"
-import { getExperiences } from "@/services/queries";
+import { getEducations, getExperiences } from "@/services/queries";
 import LoadSvgIcon from "@/utils/LoadSvgIcon";
 import { useEffect, useState } from "react";
 import ExperienceItem from '@/components/experienceItem/ExperienceItem'
 import ExperienceItemShimmer from '@/components/experienceItem/ExperienceItemShimmer'
+import EducationItem from "@/components/educationItem/EducationItem";
+import EducationItemShimmer from "@/components/educationItem/EducationItemShimmer";
 
 const Resume = () => {
 
@@ -12,6 +14,8 @@ const Resume = () => {
 
     const [experiences, setExperiences] = useState([]);
     const [loadingExp, setLoadingExp] = useState(true);
+    const [educations, setEducations] = useState([]);
+    const [loadingEdu, setLoadingEdu] = useState(true);
 
     useEffect(() => {
         getExperiences()
@@ -24,29 +28,59 @@ const Resume = () => {
                 console.log(err)
                 setLoadingExp(false)
             })
+        getEducations()
+            .then(res => {
+                console.log(res)
+                setEducations(res?.educations)
+                setLoadingEdu(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setLoadingEdu(false)
+            })
     }, [locale]);
 
     return (
         <>
-            <div className="h-full">
+            <div className="h-full flex flex-col">
                 <Title>{t.resume}</Title>
-                <div className="pt-7">
+                <div className="pt-7 flex-grow overflow-hidden overflow-y-auto no-scroll">
                     <div className="flex items-center border-b border-center-image pb-7 mb-7">
                         <LoadSvgIcon name="bag" size={32} weight={2} color="var(--color-primary)" />
                         <p className="text-base font-bold text-captionDark ms-2 capitalize">{t.experience}</p>
                     </div>
-                    {loadingExp ? (
-                        <>
-                            <ExperienceItemShimmer />
-                            <ExperienceItemShimmer />
-                        </>
-                    ) : (
-                        <>
-                            {experiences?.map(item => {
-                                return <ExperienceItem item={item} key={item?._id} />
-                            })}
-                        </>
-                    )}
+                    <div className="">
+                        {loadingExp ? (
+                            <>
+                                <ExperienceItemShimmer />
+                                <ExperienceItemShimmer />
+                            </>
+                        ) : (
+                            <>
+                                {experiences?.map(item => {
+                                    return <ExperienceItem item={item} key={item?._id} />
+                                })}
+                            </>
+                        )}
+                    </div>
+                    <div className="flex items-center border-b border-center-image pb-7 mb-7 mt-10">
+                        <LoadSvgIcon name="education" size={32} weight={2} color="var(--color-primary)" />
+                        <p className="text-base font-bold text-captionDark ms-2 capitalize">{t.education}</p>
+                    </div>
+                    <div className="">
+                        {loadingEdu ? (
+                            <>
+                                <EducationItemShimmer />
+                                <EducationItemShimmer />
+                            </>
+                        ) : (
+                            <>
+                                {educations?.map(item => {
+                                    return <EducationItem item={item} key={item?._id} />
+                                })}
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
