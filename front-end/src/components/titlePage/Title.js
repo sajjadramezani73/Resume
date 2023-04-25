398e698efdcb21fa0bd63249b989b6d4094cd474
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-25 11:45:30 +0330

    Front-end style: Add dark mode to title component

--- front-end/src/components/titlePage/Title.js
+++ front-end/src/components/titlePage/Title.js
@@ -3,7 +3,7 @@ import useTransition from '@/hooks/useTransition'
 const Title = ({ children }) => {
     const { locale } = useTransition()
     return (
-        <p className={`pb-7 text-xl font-bold text-captionDark capitalize relative border-b ${locale == 'fa' ? 'border-bottom-image-to-left' : 'border-bottom-image-to-right'} `}>
+        <p className={`pb-7 text-xl font-bold text-captionDark dark:text-lightCaptionLight capitalize relative border-b ${locale == 'fa' ? 'border-bottom-image-to-left' : 'border-bottom-image-to-right'} `}>
             {children}
             <span className="absolute top-1 -start-2.5 w-7 h-7 rounded-full bg-primary/5"></span>
         </p>
