380012d2fe3aa6d54d3dbf376846ec1befcae6ff
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-25 11:53:02 +0330

    Front-end style: Add theme dark to border bottom title component

--- front-end/src/components/titlePage/Title.js
+++ front-end/src/components/titlePage/Title.js
@@ -1,9 +1,13 @@
 import useTransition from '@/hooks/useTransition'
+import { useThemeAction } from '@/store/themeSlice'
 
 const Title = ({ children }) => {
     const { locale } = useTransition()
+    const { theme } = useThemeAction()
     return (
-        <p className={`pb-7 text-xl font-bold text-captionDark dark:text-lightCaptionLight capitalize relative border-b ${locale == 'fa' ? 'border-bottom-image-to-left' : 'border-bottom-image-to-right'} `}>
+        <p className={`pb-7 text-xl font-bold text-captionDark dark:text-lightCaptionLight capitalize relative border-b 
+        ${locale == 'fa' ? (theme === 'dark' ? 'border-bottom-image-to-left-dark' : 'border-bottom-image-to-left') :
+                (theme === 'dark' ? 'border-bottom-image-to-right-dark' : 'border-bottom-image-to-right')} `}>
             {children}
             <span className="absolute top-1 -start-2.5 w-7 h-7 rounded-full bg-primary/5"></span>
         </p>
