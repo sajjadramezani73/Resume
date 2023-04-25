ae1294294839de1bde60617f99cb2ee8d7b36176
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-25 12:57:47 +0330

    Front-end style: Add theme dark for project page

--- front-end/src/components/projectItem/ProjectItem.js
+++ front-end/src/components/projectItem/ProjectItem.js
@@ -1,21 +1,24 @@
 import useTransition from '@/hooks/useTransition'
 import Button from '../ui/button/Button'
+import { useThemeAction } from '@/store/themeSlice'
 
 const ProjectItem = ({ item }) => {
 
+    const { theme } = useThemeAction()
     const { t } = useTransition()
 
     return (
-        <div className='flex mb-4 pb-4 border-b border-center-image last:border-0'>
+        <div className={`flex mb-4 pb-4 border-b last:border-0 
+        ${theme === 'dark' ? 'border-center-image-dark' : 'border-center-image'}`}>
             <div className="w-44 min-w-[176px] h-48 rounded border overflow-hidden"></div>
             <div className="flex-grow px-3 pt-1">
-                <p className='font-semibold text-captionDark mb-3 capitalize'>{item?.title}</p>
-                <p className='text-caption text-xs font-medium leading-6 capitalize'>{item?.briefDescription}</p>
+                <p className='font-semibold text-captionDark dark:text-lightCaptionLight mb-3 capitalize'>{item?.title}</p>
+                <p className='text-caption dark:text-lightCaption text-xs font-medium leading-6 capitalize'>{item?.briefDescription}</p>
                 <div className="flex flex-wrap gap-x-2 gap-y-1 mt-2">
                     {item?.skills?.map(item => {
                         return <span
                             key={item}
-                            className='w-max bg-gray-200 text-caption text-xxs font-medium px-1.5 pt-1 pb-0.5 rounded capitalize'
+                            className='w-max bg-gray-200 dark:bg-lightCaptionLight/70 text-caption text-xxs font-medium px-1.5 pt-1 pb-0.5 rounded capitalize'
                         >
                             {item}
                         </span>
--- front-end/src/components/projectItem/ProjectItemShimmer.js
+++ front-end/src/components/projectItem/ProjectItemShimmer.js
@@ -1,21 +1,25 @@
+import { useThemeAction } from "@/store/themeSlice"
 
 const ProjectItemShimmer = ({ item }) => {
 
+    const { theme } = useThemeAction()
+
     return (
-        <div className='flex mb-4 pb-4 border-b border-center-image last:border-0'>
-            <div className="w-44 min-w-[176px] h-48 rounded border bg-gray-100 animate-pulse"></div>
+        <div className={`flex mb-4 pb-4 border-b last:border-0
+        ${theme === 'dark' ? 'border-center-image-dark' : 'border-center-image'}`}>
+            <div className="w-44 min-w-[176px] h-48 rounded bg-gray-100 dark:bg-gray-50/20 animate-pulse"></div>
             <div className="flex-grow px-3 pt-1">
-                <div className='w-40 h-5 rounded-sm mb-3 bg-gray-100 animate-pulse'></div>
-                <div className='w-full h-5 rounded-sm bg-gray-100 animate-pulse'></div>
-                <div className='w-full h-5 rounded-sm bg-gray-100 animate-pulse mt-2'></div>
-                <div className='w-1/2 h-5 rounded-sm bg-gray-100 animate-pulse mt-2'></div>
+                <div className='w-40 h-5 rounded-sm mb-3 bg-gray-100 dark:bg-gray-50/20 animate-pulse'></div>
+                <div className='w-full h-5 rounded-sm bg-gray-100 dark:bg-gray-50/20 animate-pulse'></div>
+                <div className='w-full h-5 rounded-sm bg-gray-100 dark:bg-gray-50/20 animate-pulse mt-2'></div>
+                <div className='w-1/2 h-5 rounded-sm bg-gray-100 dark:bg-gray-50/20 animate-pulse mt-2'></div>
                 <div className="flex flex-wrap gap-x-2 gap-y-1 mt-2">
                     {[1, 2, 3, 4, 5].map(item => {
-                        return <span className='w-16 h-6 rounded-sm bg-gray-100 animate-pulse'></span>
+                        return <span className='w-16 h-6 rounded-sm bg-gray-100 dark:bg-gray-50/20 animate-pulse'></span>
                     })}
                 </div>
                 <div className="flex justify-end pt-4">
-                    <div className="w-28 h-8 rounded bg-gray-100 animate-pulse"></div>
+                    <div className="w-28 h-8 rounded bg-gray-100 dark:bg-gray-50/20 animate-pulse"></div>
                 </div>
             </div>
         </div>
