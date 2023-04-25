bfa3f35bddf021fd295f8ffa1ea430d04f4390fa
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-25 11:59:51 +0330

    Front-end style: Add dark mode to about page

--- front-end/src/containers/about/About.js
+++ front-end/src/containers/about/About.js
@@ -2,10 +2,12 @@ import Title from "@/components/titlePage/Title"
 import useTransition from "@/hooks/useTransition"
 import { useAboutAction } from "@/store/aboutSlice"
 import AboutItem from "./components/AboutItem"
+import { useThemeAction } from "@/store/themeSlice"
 
 const About = () => {
-    const { t } = useTransition()
 
+    const { theme } = useThemeAction()
+    const { t } = useTransition()
     const { aboutMe } = useAboutAction()
 
     return (
@@ -13,8 +15,8 @@ const About = () => {
             <div className="h-full">
                 <Title>{t.about}</Title>
                 <div className="grid grid-cols-2">
-                    <div className="pt-7 border-e border-vertical-image pe-6">
-                        <p className="text-base text-caption font-medium leading-8 capitalize">{aboutMe?.bio}</p>
+                    <div className={`pt-7 border-e pe-6 ${theme === 'dark' ? 'border-vertical-image-dark' : 'border-vertical-image'}`}>
+                        <p className="text-base text-caption dark:text-lightCaption font-medium leading-8 capitalize">{aboutMe?.bio}</p>
                     </div>
                     <div className="pt-7 ps-6 grid gap-y-3">
                         <AboutItem title={t.age} value={aboutMe?.age} />
--- front-end/src/containers/about/components/AboutItem.js
+++ front-end/src/containers/about/components/AboutItem.js
@@ -1,9 +1,12 @@
+import { useThemeAction } from "@/store/themeSlice"
 
 const AboutItem = ({ title, value }) => {
+    const { theme } = useThemeAction()
     return (
-        <div className='flex justify-between items-center border-b border-center-image pb-3 last:border-0'>
+        <div className={`flex justify-between items-center border-b pb-3 last:border-0
+        ${theme === 'dark' ? 'border-center-image-dark' : 'border-center-image'}`}>
             <p className="text-tiny font-medium text-primary capitalize">{title}</p>
-            <p className="text-tiny font-medium text-caption">{value}</p>
+            <p className="text-tiny font-medium text-caption dark:text-lightCaption">{value}</p>
         </div>
     )
 }
