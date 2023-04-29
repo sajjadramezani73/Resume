1dbe50cfb2cc8a1994b188d2d3ad7b12e6e931de
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-29 13:31:49 +0330

    Front-end Responsive: Responsive resume page
    responsive Experience and education components

--- front-end/src/components/experienceItem/ExperienceItem.js
+++ front-end/src/components/experienceItem/ExperienceItem.js
@@ -1,8 +1,8 @@
 
 const experienceItem = ({ item }) => {
     return (
-        <div className='w-full flex group'>
-            <div className="w-44 min-w-[176px] text-end">
+        <div className='w-full md:flex grid grid-flow-col justify-start group'>
+            <div className="md:w-44 md:min-w-[176px] md:text-end">
                 <p className="text-xs text-captionDark dark:text-lightCaptionLight/70 font-semibold pt-0.5">{item?.dateStart} - {item?.dateEnd}</p>
                 <a
                     href={item?.companyLink}
@@ -12,10 +12,10 @@ const experienceItem = ({ item }) => {
                     {item?.company}
                 </a>
             </div>
-            <div className="border-r relative mx-6">
+            <div className="w-fit border-r relative mx-6 ms-2.5 md:ms-6 -order-1 md:order-none row-span-2">
                 <span className="absolute top-1.5 left-1/2 -translate-x-1/2 w-[10px] h-[10px] bg-white rounded-full border-2 border-primary ring-[5px] ring-primary/30"></span>
             </div>
-            <div className="pb-7 group-last:pb-0 w-full">
+            <div className="pb-7 pt-2 md:pt-0 group-last:pb-0 w-full">
                 <p className="text-base text-captionDark dark:text-lightCaptionLight font-semibold capitalize">{item?.title}</p>
                 <p className="text-sm text-caption dark:text-lightCaption mt-2">{item?.description}</p>
                 <div className="flex flex-wrap gap-x-2 gap-y-1 mt-2">
